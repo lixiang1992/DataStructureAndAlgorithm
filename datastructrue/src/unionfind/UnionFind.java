7297606826fbe94dde23bde879632495ee919e73
lixiang1992 <275005161@qq.com>
2022-07-30 23:40:43 +0800

    并查集模板

---
+++ datastructrue/src/unionfind/UnionFind.java
@@ -0,0 +1,39 @@
+package unionfind;
+
+/**
+ * 并查集模板，包含路径压缩和按秩合并
+ */
+public class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        rank = new int[n];
+    }
+
+    public void union(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            if (rank[rootx] > rank[rooty]) {
+                parent[rooty] = rootx;
+            } else if (rank[rootx] < rank[rooty]) {
+                parent[rootx] = rooty;
+            } else {
+                parent[rooty] = rootx;
+                rank[rootx]++;
+            }
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
